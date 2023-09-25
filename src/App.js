127c82aefb97c83695a5553fb581363743b1ac7c
Vlada Moroz <vladochkamoroz2812@gmail.com>
2023-09-25 14:57:10 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
+import "react-toastify/dist/ReactToastify.css";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import Shops from "./pages/Shops";
+import Cart from "./pages/Cart";
+import Home from "./pages/Home";
+import ClientForm from "./pages/ClientForm";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+const Layout = () => {
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar/>
+      <Outlet />
+      <Footer/>
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: `/shops`,
+        element: <Shops />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+      {
+        path: "/cart/orderdetails",
+        element: <ClientForm />,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ToastContainer />
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+import { disableReactDecTools } from "@fvilers/disable-react-devtools";
+import "./index.css";
+import CartSlice, { getTotal } from "./components/redux/CartSlice";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+//if (process.env.NODE_ENV === "production") disableReactDecTools();
+
+const store = configureStore({
+  reducer: {
+    cart: CartSlice,
+  },
+});
+store.dispatch(getTotal());
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
